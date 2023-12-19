703da4a474680bd589779bc7a24747ab87e94d85
LeoGidev <leogimenez.dev@gmail.com>
2023-12-18 21:22:06 -0300

    trabajamos en el codigo

--- index.php
+++ index.php
@@ -14,3 +14,6 @@ if (!$conn) {
 $datos = mysqli_query($conn,"SELECT*FROM Temperatura ");
 /////////////////obtener el valor de la tabla, debemos nombrarla como Temperatura en la base de datos
 $coef=12;
+$y1h=0; //variable y para  lineas horizontales
+$x1h=0;//variable x para  lineas horizontales
+$yn=0;//variable y para numeros de eje uptime(y)
